edacda69555c5cc68036017dd3edad62ff3e0cad
Nasker <xamps23@gmail.com>
2022-05-09 09:57:04 +0200

    Implementing notes player functions

--- ATLASDAN/RTPBuit/src/Helpers/NotesPlayer.cpp
+++ ATLASDAN/RTPBuit/src/Helpers/NotesPlayer.cpp
@@ -6,21 +6,38 @@ NotesPlayer::NotesPlayer(){
 }
 
 void NotesPlayer::queueNotes(RTPEventNotePlus note){
-
+    _notesQueue.push(note);
 }
 
 void NotesPlayer::playNotes(){
-
+    while(!_notesQueue.empty()){
+        RTPEventNotePlus note = _notesQueue.front();
+        _notesQueue.pop();
+        _ringingNotes[note.getMidiChannel()*1000 + note.getEventNote()] = note;
+        note.playNoteOn();
+    }
 }
 
 void NotesPlayer::decreaseTimeToLive(){
-
+    std::map<int, RTPEventNotePlus>::iterator it;
+    for(it = _ringingNotes.begin(); it != _ringingNotes.end(); it++){
+        it->second.decreaseTimeToLive();
 }
 
 bool NotesPlayer::killThatNote(int keyToNote){
-
+    std::map<int, RTPEventNotePlus>::iterator it;
+    it = _ringingNotes.find(keyToNote);
+    if(it != _ringingNotes.end()){
+        it->second.playNoteOff();
+        _ringingNotes.erase(it);
+        return true;
+    }
+    return false;
 }
 
 void NotesPlayer::killAllNotes(){
-
+    std::map<int, RTPEventNotePlus>::iterator it;
+    for(it = _ringingNotes.begin(); it != _ringingNotes.end(); it++)
+        it->second.playNoteOff();
+    _ringingNotes.clear();
 }
