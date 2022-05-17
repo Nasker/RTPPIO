462ea4b42db646a89a08d9fb5e10c6a252b1db10
omartinez <omartinez@ifae.es>
2022-05-17 15:55:21 +0200

    slight edit

--- ATLASDAN/RTPBuit/src/BuitFunctions/MatrixBuitControlChange.cpp
+++ ATLASDAN/RTPBuit/src/BuitFunctions/MatrixBuitControlChange.cpp
@@ -1,19 +1,21 @@
 #include "MatrixBuitControlChanger.hpp"
 
 MatrixBuitControlChanger::MatrixBuitControlChanger(){
-    for(int i=0; i < N_BUITS_CC; i++){
+    for(int i=0; i < N_BUITS_CC; i++)
         controlChangers.push_back(BuitControlChanger(i));
-    } 
 }
+
 void MatrixBuitControlChanger::toggleBuitCC(int buitID){
     controlChangers[buitID].isEnabled() ? controlChangers[buitID].disable() : controlChangers[buitID].enable();
 }
+
 RTPSequencesState MatrixBuitControlChanger::getBuitCCStates(){
     RTPSequencesState sequencesState;
     for(int i=0; i < N_BUITS_CC; i++)
         sequencesState.sequenceState[i].state = controlChangers[i].isEnabled();
     return sequencesState;
 }
+
 void MatrixBuitControlChanger::updateAndSend(ControlCommand command){
     for(int i=0; i < N_BUITS_CC; i++){
         if(controlChangers[i].isEnabled())
